62461cef3384f5f90cfea30035b62246c1cc4040
gale46 <113490808+gale46@users.noreply.github.com>
2023-01-05 14:37:36 +0800

    Update 02.py

--- 02.py
+++ 02.py
@@ -40,12 +40,19 @@ for i in range(10):
   t[:,:,i] = noise.random_noise(g, 'gaussian', var = 0.07)
 #去除高斯雜訊，計算平均值，將10張圖相加/10
 #2=二維
+# mean()函数功能：求取均值
 ta = np.mean(t, 2)
 figure(), io.imshow(ta)
 
 cr = io.imread(r'/content/drive/MyDrive/car.png')
 #動態濾波器:10*1 element=0.1陣列
 #進行捲積，圖片模糊
+'''
+>>> s = (2,2)
+>>> np.ones(s)
+array([[1.,  1.],
+       [1.,  1.]])
+'''
 m = np.ones((10,1))/10
 cm = ndi.correlate(cr,m)
 #去除動態模糊
@@ -59,6 +66,8 @@ mf2 = fft2(m2)
 
 d = 0.02
 
+
+#d越大白色越多
 
 mf2[np.where(abs(mf2)<d)] = 1
 bmi2 = abs(ifft2(fft2(cm)/mf2))
